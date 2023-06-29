5e7a8b20849e4ecb4f43f7c743374617e067fe8a
JDX50S <71799746+smoke-wolf@users.noreply.github.com>
2023-06-28 20:26:22 -0700

    Update TouchEditor.py

--- TouchEditor.py
+++ TouchEditor.py
@@ -2,9 +2,10 @@ import re
 import sys
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTextEdit, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTextEdit, QFileDialog, QPushButton
 from PyQt5.QtGui import QIcon, QTextCursor, QTextCharFormat, QColor, QKeySequence
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize
+
 
 class TouchScriptEditor(QMainWindow):
     def __init__(self):
@@ -16,28 +17,53 @@ class TouchScriptEditor(QMainWindow):
         self.setWindowTitle('Touch Script Editor')
 
         # Create actions
-        new_file_action = QAction(QIcon('icons/new.png'), 'New', self)
+        new_file_action = QAction('New', self)
         new_file_action.setShortcut(QKeySequence.New)
         new_file_action.triggered.connect(self.new_file)
 
-        open_file_action = QAction(QIcon('icons/open.png'), 'Open', self)
+        open_file_action = QAction('Open', self)
         open_file_action.setShortcut(QKeySequence.Open)
         open_file_action.triggered.connect(self.open_file)
 
-        save_file_action = QAction(QIcon('icons/save.png'), 'Save', self)
+        save_file_action = QAction('Save', self)
         save_file_action.setShortcut(QKeySequence.Save)
         save_file_action.triggered.connect(self.save_file)
 
-        save_as_file_action = QAction(QIcon('icons/save-as.png'), 'Save As', self)
+        save_as_file_action = QAction('Save As', self)
         save_as_file_action.setShortcut(QKeySequence.SaveAs)
         save_as_file_action.triggered.connect(self.save_as_file)
 
         # Create toolbar
         toolbar = self.addToolBar('Toolbar')
+        toolbar.setMovable(False)
+        toolbar.setStyleSheet("QToolBar {border: none;}")
+        toolbar.setIconSize(QSize(32, 32))
+        toolbar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
         toolbar.addAction(new_file_action)
         toolbar.addAction(open_file_action)
         toolbar.addAction(save_file_action)
         toolbar.addAction(save_as_file_action)
+
+        # Create buttons
+        new_button = QPushButton("New", self)
+        new_button.move(10, 30)
+        new_button.resize(70, 30)
+        new_button.clicked.connect(self.new_file)
+
+        open_button = QPushButton("Open", self)
+        open_button.move(90, 30)
+        open_button.resize(70, 30)
+        open_button.clicked.connect(self.open_file)
+
+        save_button = QPushButton("Save", self)
+        save_button.move(170, 30)
+        save_button.resize(70, 30)
+        save_button.clicked.connect(self.save_file)
+
+        save_as_button = QPushButton("Save As", self)
+        save_as_button.move(250, 30)
+        save_as_button.resize(70, 30)
+        save_as_button.clicked.connect(self.save_as_file)
 
         # Create text editor
         self.editor = QTextEdit()
@@ -47,13 +73,13 @@ class TouchScriptEditor(QMainWindow):
 
         # Highlighting rules
         self.highlighting_rules = []
-        self.highlighting_rules.append((r'\bwait\b\s+arg\s*\(\s*(\d+)\s*\)', QColor('#56B6C2')))
-        self.highlighting_rules.append((r'\bhold\b\s+args\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)', QColor('#F24B3C')))
-        self.highlighting_rules.append((r'\bclick\b\s+args\s*\(\s*(\d+)\s*,\s*(\d+)\s*\)', QColor('#FFC107')))
-        self.highlighting_rules.append((r'\bswipe\b\s+args\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)', QColor(0, 255, 45)))
-        self.highlighting_rules.append((r'\bdoubleclick\b\s+args\s*\(\s*(\d+)\s*,\s*(\d+)\s*\)', QColor(0, 255, 0)))
-        self.highlighting_rules.append((r'\btype\b\s+([^\n]+)', QColor(0, 0, 255)))
-        self.highlighting_rules.append((r'\bhotkey\b\s+([^\n]+)', QColor(255, 0, 0)))
+        self.highlighting_rules.append((r'\bwait\b', QColor('#56B6C2')))
+        self.highlighting_rules.append((r'\bhold\b', QColor('#F24B3C')))
+        self.highlighting_rules.append((r'\bclick\b', QColor('#FFC107')))
+        self.highlighting_rules.append((r'\bswipe\b', QColor(0, 255, 45)))
+        self.highlighting_rules.append((r'\bdoubleclick\b', QColor(0, 255, 0)))
+        self.highlighting_rules.append((r'\btype\b', QColor(250, 0, 100)))
+        self.highlighting_rules.append((r'\bhotkey\b', QColor(255, 0, 0)))
 
         # Set default font
         font = self.editor.document().defaultFont()
@@ -67,54 +93,62 @@ class TouchScriptEditor(QMainWindow):
         self.editor.clear()
 
     def open_file(self):
-        file_name, _ = QFileDialog.getOpenFileName(self, 'Open file', '', 'Touch Script (.tch);;All Files (.*)')
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open File', '', 'Touch Script (*.touch)')
         if file_name:
             with open(file_name, 'r') as f:
-                file_contents = f.read()
-                self.editor.setPlainText(file_contents)
+                file_content = f.read()
+                self.editor.setPlainText(file_content)
 
     def save_file(self):
-        if self.current_file:
-            self.save_to_file(self.current_file)
+        file_name = getattr(self, 'file_name', None)
+        if file_name:
+            with open(file_name, 'w') as f:
+                f.write(self.editor.toPlainText())
         else:
             self.save_as_file()
 
     def save_as_file(self):
-        file_name, _ = QFileDialog.getSaveFileName(self, 'Save As', '', 'Touch Script (*.tch);;All Files (*.*)')
+        file_name, _ = QFileDialog.getSaveFileName(self, 'Save File As', '', 'Touch Script (*.touch)')
         if file_name:
-            self.current_file = file_name
-            self.save_to_file(self.current_file)
-
-    def save_to_file(self, file_name):
-        with open(file_name, 'w') as f:
-            f.write(self.editor.toPlainText())
+            self.file_name = file_name
+            self.save_file()
 
     def highlight_current_line(self):
-        selection = QTextEdit.ExtraSelection()
-        line_color = QColor(Qt.yellow).lighter(160)
-        selection.format.setBackground(line_color)
-        selection.format.setProperty(QTextCharFormat.FullWidthSelection, True)
-        selection.cursor = self.editor.textCursor()
-        selection.cursor.clearSelection()
-        self.editor.setExtraSelections([selection])
-
-        # Highlight syntax
-        for pattern, color in self.highlighting_rules:
-            regex = re.compile(pattern)
-            matches = regex.finditer(self.editor.toPlainText())
-            for match in matches:
-                start = match.start()
-                end = match.end()
-                self.highlight(start, end, color)
-
-    def highlight(self, start, end, color):
         cursor = self.editor.textCursor()
-        cursor.setPosition(start)
-        cursor.movePosition(QTextCursor.Right, QTextCursor.KeepAnchor, end - start)
-        format = QTextCharFormat()
-        format.setBackground(color)
-        cursor.setCharFormat(format)
+        line_number = cursor.blockNumber()
+        block = cursor.block()
+        text = block.text()
+
+        # Highlighting rules
+        color = QColor('#FFFFFF')  # Default color
+        for pattern, rule_color in self.highlighting_rules:
+            match = re.search(pattern, text)
+            if match:
+                color = rule_color
+
+        # Set current line text color
+        fmt = QTextCharFormat()
+        fmt.setForeground(color)
+        cursor.select(QTextCursor.LineUnderCursor)
+        cursor.mergeCharFormat(fmt)
+        cursor.clearSelection()
+
+    # Auto-complete
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Tab:
+            cursor = self.editor.textCursor()
+            cursor.movePosition(QTextCursor.StartOfLine)
+            cursor.select(QTextCursor.LineUnderCursor)
+            line = cursor.selectedText().strip()
+
+            for rule in self.highlighting_rules:
+                if rule[0] in line:
+                    command = rule[0].replace('\\b', '').replace('\\b', '').strip()
+                    self.editor.insertPlainText('\n' + command + ' ')
+                    return
+
+        super().keyPressEvent(event)
 
 app = QApplication(sys.argv)
-ex = TouchScriptEditor()
+window = TouchScriptEditor()
 sys.exit(app.exec_())
